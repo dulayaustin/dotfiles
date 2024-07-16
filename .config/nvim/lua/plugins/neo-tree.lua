14178f77eea0b3788fc2c3ed72b2986e86fb622a
Austin Dulay <dulayaustin@gmail.com>
2024-07-17 00:42:03 +0800

    Update nvim neotree configuration options

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,34 +1,27 @@
 return {
 	"nvim-neo-tree/neo-tree.nvim",
-	keys = {
-		{ "<C-n>", "<CMD>Neotree filesystem reveal toggle<CR>" },
-    { "<leader>fb", "<CMD>Neotree buffers reveal float<CR>" },
-	},
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-
-	--	config = function()
-	--		vim.keymap.set("n", "<C-n>", ":Neotree reveal toggle<CR>", {})
-	--
-	----		require("neo-tree").setup({
-	----			event_handlers = {
-	----				{
-	----					event = "file_open_requested",
-	----					handler = function()
-	----						require("neo-tree.command").execute({ action = "close_all_nodes" })
-	----					end,
-	----				},
-	----			},
-	----			filesystem = {
-	----				follow_current_file = {
-	----					enabled = true,
-	----					leave_dirs_open = false,
-	----				},
-	----			},
-	----		})
-	--	end,
+	keys = {
+		{ "<C-n>", "<CMD>Neotree filesystem reveal toggle<CR>" },
+		{ "<leader>fb", "<CMD>Neotree buffers reveal float<CR>" },
+	},
+	opts = {
+		close_if_last_window = true,
+		window = {
+			mappings = {
+				["Z"] = "close_all_nodes",
+			},
+		},
+		filesystem = {
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = false,
+			},
+		},
+	},
 }
