23d5eee34a5870c0696bf3757d2e4f2d6775e163
Austin Dulay <dulayaustin@gmail.com>
2024-07-20 01:36:53 +0800

    Add view-rails for easy navigation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -8,7 +8,10 @@ return {
 
 		configs.setup({
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = true,
+			},
 			indent = { enable = true },
 		})
 	end,
